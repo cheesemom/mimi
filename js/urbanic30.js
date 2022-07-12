139cc5dad996a99f535bf9740dba9901b0b685c1
cheesemom <smsm2610@naver.com>
2022-07-12 17:58:20 +0900

    0712

---
+++ js/urbanic30.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        autoplay: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+    });
+
+
+})
