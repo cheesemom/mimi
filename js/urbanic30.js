be19f39bdec7183a4adc84fdbaea1f66fe6b45aa
cheesemom <smsm2610@naver.com>
2022-07-15 14:26:35 +0900

    my HBD

--- js/urbanic30.js
+++ js/urbanic30.js
@@ -4,6 +4,7 @@ $(function () {
         // Optional parameters
         direction: 'vertical',
         autoplay: true,
+        autoplaySpeed: 2000,
 
         // If we need pagination
         pagination: {
@@ -12,10 +13,33 @@ $(function () {
         },
     });
 
+
+
     $('.Top').on('click', function () {
         $('html,body').animate({ scrollTop: 0 }, 200)
     });
 
 
 
-})
+    $('.main_slider').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '300px',
+        slidesToShow: 2,
+        autoplaySpeed: 2000,
+    })
+
+
+    AOS.init();
+
+});
+
+
+
+
+
+
+
