146db46b2078fa6fad33bafe803e0cc1666026f8
cheesemom <smsm2610@naver.com>
2022-07-21 17:50:24 +0900

    0721

---
+++ js/pet.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+
+    $('.mainArrow i:nth-child(1)').on('click', function () {
+        $('.main_slider').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function () {
+        $('.main_slider').slick('slickNext')
+    });
+
+
+
+
+
+
+
+
+
+})
