bfb1c5aee10e89f6b6c844f1355b8416ba34220d
cheesemom <smsm2610@naver.com>
2022-07-27 17:47:49 +0900

    0727

--- js/juliette.js
+++ js/juliette.js
@@ -5,8 +5,39 @@ $(function () {
     });
 
 
+
+
+
+    $('.new02').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 0,
+        speed: 3000,
+        slidesToShow: 4,
+        cssEase: "linear",
+    });
+
+
+
+
+
     AOS.init();
 
+
+
+
+
+    var scroll_menu = $('#scroll_menu').offset().top;
+    $(window).scroll(function () {
+        var window = $(this).scrollTop();
+        if (scroll_menu <= window) {
+            $('#scroll_menu').addClass("fixed");
+        } else {
+            $('#scroll_menu').removeClass("fixed");
+        }
+    })
 
 
 
