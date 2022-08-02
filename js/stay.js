107abbfb29b5dcd6f243c1cc283eace784e745f2
cheesemom <smsm2610@naver.com>
2022-08-02 17:46:54 +0900

    0802

--- js/stay.js
+++ js/stay.js
@@ -10,6 +10,37 @@ $(function () {
 
 
 
+    $('.Mijas_slider').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 4000,
+        dots: true,
+    });
+
+
+
+    $('.Palma_slider').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 4000,
+        dots: true,
+    });
+
+
+
+
+
+
+
+
+
+
+
+    AOS.init();
 
 
 
