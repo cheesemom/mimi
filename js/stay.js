a1d1a8df38bcee8250f4fb3362bc4d058a4b813d
cheesemom <smsm2610@naver.com>
2022-08-05 10:30:38 +0900

    0805

--- js/stay.js
+++ js/stay.js
@@ -23,13 +23,31 @@ $(function () {
     });
 
 
-
-    $('.bxSlider2').bxSlider({
-        slideWidth: 600,
-        auto: true,
-        autoControls: true,
-        stopAutoOnclick: true,
-        autoplaySpeed: 2000,
+    $('.tap_menu>li>a').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $('.tab_content>li').removeClass('on')
+        $('.tab_content>li').eq(idx).addClass('on');
+        $('.tap_menu>li').removeClass('on')
+        $(this).parent().addClass('on');
     });
+
+
+    $('.living_slider').slick({
+        slidesToShow: 4,
+    });
+
+    $('.kitchen_slider').slick({
+        slidesToShow: 4,
+    });
+
+    $('.bath_slider').slick({
+        slidesToShow: 4,
+    });
+
+    $('.pool_slider').slick({
+        slidesToShow: 4,
+    });
+
 
 })
