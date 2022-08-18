1195d06cd4d938d381d59593bbb4b18b07f46fbb
cheesemom <smsm2610@naver.com>
2022-08-18 17:45:17 +0900

    0818

--- js/starbucks.js
+++ js/starbucks.js
@@ -1,8 +1,20 @@
 $(function () {
 
+    //푸터 슬라이드
+    new Swiper('.awards .swiper', {
+        autoplay: true,
+        loop: true,
+        slidesPerView: 5,
+        navigation: {
+            prevEl: '.awards .swiper-button-prev',
+            nextEl: '.awards .swiper-button-next'
+        }
+    });
 
 
 
-
+    // 올해 몇년도
+    const thisYear = document.querySelector('.this-year');
+    thisYear.textContent = new Date().getFullYear();
 
 })
