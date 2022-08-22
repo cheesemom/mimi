d1e14670dd27ca6e1b2c27e2fe68398bc3849929
cheesemom <smsm2610@naver.com>
2022-08-22 17:47:18 +0900

    0822

--- js/starbucks.js
+++ js/starbucks.js
@@ -1,14 +1,43 @@
 $(function () {
 
-    //푸터 슬라이드
-    new Swiper('.awards .swiper', {
+    //프로모션 슬라이드
+    new Swiper('.notice .swiper', {
+        direction: 'vertical', //수직 슬라이드
         autoplay: true,
         loop: true,
-        slidesPerView: 5,
-        navigation: {
-            prevEl: '.awards .swiper-button-prev',
-            nextEl: '.awards .swiper-button-next'
+    });
+
+    new Swiper('.promotion .swiper', {
+        direction: 'horizontal',
+        autoplay: {
+            delay: 5000
+        },
+        loop: true,
+        slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+        spaceBetween: 10, // 슬라이드 사이 여백
+        centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+        pagination: { // 페이지 번호 사용
+            el: '.promotion .swiper-pagination', // 페이지 번호 요소
+            clickable: true // 사용자의 페이지 번호 제어 여부
+        },
+        navigation: { // 슬라이드 이전/다음 버튼 사용
+            prevEl: '.promotion .swiper-button-prev', // 이전 버튼 요소
+            nextEl: '.promotion .swiper-button-next' // 다음 버튼 요소
         }
+    });
+
+
+
+    // 스크롤매직
+    const spyEls = document.querySelectorAll('section.scroll-spy');
+    spyEls.forEach(function (spyEl) {
+        new ScrollMagic
+            .Scene({ // 감시할 장면(Scene)을 추가
+                triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+                triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+            })
+            .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+            .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
     });
 
 
@@ -16,5 +45,7 @@ $(function () {
     // 올해 몇년도
     const thisYear = document.querySelector('.this-year');
     thisYear.textContent = new Date().getFullYear();
+
+
 
 })
