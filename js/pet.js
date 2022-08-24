093ff81d26cdcfe17ad700d5d3382ab2ec27d2ca
cheesemom <smsm2610@naver.com>
2022-08-24 17:51:00 +0900

    0824

--- js/pet.js
+++ js/pet.js
@@ -5,31 +5,20 @@ $(function () {
     });
 
 
-    $('.main_slider').slick({
-        arrows: false,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
-
-
-    $('.mainArrow i:nth-child(1)').on('click', function () {
-        $('.main_slider').slick('slickPrev')
-    });
-    $('.mainArrow i:nth-child(2)').on('click', function () {
-        $('.main_slider').slick('slickNext')
-    });
-
-
-
-    new Swiper('.main_visual04 .swiper', {
+    new Swiper('.main_visual .swiper', {
         direction: 'horizontal',
         autoplay: {
-            delay: 3000
+            delay: 4000
         },
         loop: true,
-        slidesPerView: 1,
-
+        pagination: { // 페이지 번호 사용
+            el: '.main_visual .swiper-pagination', // 페이지 번호 요소
+            clickable: true // 사용자의 페이지 번호 제어 여부
+        },
+        navigation: { // 슬라이드 이전/다음 버튼 사용
+            prevEl: '.main_visual .swiper-button-prev',
+            nextEl: '.main_visual .swiper-button-next'
+        }
     });
 
 
