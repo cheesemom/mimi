4e1afd05df8db3019573ffdba26354de75d7ae91
cheesemom <smsm2610@naver.com>
2022-09-08 17:01:37 +0900

    0908

---
+++ js/Highcheeks.js
@@ -0,0 +1,21 @@
+$(function () {
+
+
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 3000,
+    });
+
+
+
+
+
+
+
+
+
+
+})
