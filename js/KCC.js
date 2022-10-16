191a5ccc18982e1e14c6268550ae550ff44ac3f2
cheesemom <smsm2610@naver.com>
2022-10-17 01:11:22 +0900

    1017

--- js/KCC.js
+++ js/KCC.js
@@ -1,13 +1,9 @@
 $(function () {
 
-    var createNum = 0;
 
+    //main slide
     $('.main_slider').on('init afterChange', function (e, s, c) {
         $('.main_slider figure.slick-current').addClass('on').siblings().removeClass('on');
-        // $('.num').css({ backgroundPositionY: -100 * createNum + "px" });
-        // $('.custom_dots li').eq(c).addClass('on').siblings().removeClass('on');
-        // createNum++;
-        $('.main_visual01 .info_wrap .inner ul>li').eq(c).show().siblings().hide();
     })
 
     $('.main_slider').slick({
@@ -18,7 +14,45 @@ $(function () {
         autoplaySpeed: 4000,
     });
 
+    $('.lang strong').on('click', function () {
+        $(this).toggleClass('on');
+        $('.lang').toggleClass('on');
+    });
 
+    $('.top_search strong').on('click', function () {
+        $(this).toggleClass('on');
+        $('.top_search').toggleClass('on');
+    })
+
+
+    //num slide
+    $('.main_slider').on('init reInit afterChange', function (e, ms, c) {
+        var current = $('.main_slider .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.slide_dots li').eq(c).addClass('on').siblings().removeClass('on');
+        $('.main_visual .num').html('0' + ((c ? c : 0) + 1) + '<span> / 0' + ms.slideCount + '</span>');
+    });
+
+
+    //slide_dots slide
+    $('.main_visual .slide_dots li').on('click', function () {
+        var idx = $(this).index();
+        console.log(idx)
+        $('.main_slider').slick('slickGoTo', idx)
+    })
+
+
+    //arrow slide
+    $('.main_visual .slide_handler button:first-child').on('click', function () {
+        $('.main_slider').slick('slickPrev')
+    })
+    $('.main_visual .slide_handler button:last-child').on('click', function () {
+        $('.main_slider').slick('slickNext')
+    })
+
+
+
+    //feild slide
     $('.slider_wrap').slick({
         arrows: false,
         autoplay: true,
@@ -27,20 +61,50 @@ $(function () {
         autoplaySpeed: 4000,
     });
 
-    $('.arrows03 i:nth-child(1)').on('click', function () {
+    $('.arrows i:nth-child(1)').on('click', function () {
         $('.slider_wrap').slick('slickPrev')
     });
-    $('.arrows03 i:nth-child(2)').on('click', function () {
+    $('.arrows i:nth-child(2)').on('click', function () {
         $('.slider_wrap').slick('slickNext')
     });
 
 
-    $('.mainArrow i:nth-child(1)').on('click', function () {
-        $('.main_slider').slick('slickPrev')
+
+    //innovation tab slide
+    $('.tab_menu>div>a').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $('.tab_content>div').removeClass('on')
+        $('.tab_content>div').eq(idx).addClass('on');
+        $('.tap_menu>div').removeClass('on')
+        $(this).parent().addClass('on');
     });
-    $('.mainArrow i:nth-child(2)').on('click', function () {
-        $('.main_slider').slick('slickNext')
+
+    $('.work').slick({
+        slidesToShow: 3,
+        arrows: false,
     });
+
+    $('.factory').slick({
+        slidesToShow: 3,
+        arrows: false,
+    });
+
+    $('.education').slick({
+        slidesToShow: 3,
+        arrows: false,
+    });
+
+
+
+
+
+
+
+
+
+
+
 
 
     $('.toTop').on('click', function () {
